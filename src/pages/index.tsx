0e6cf4ed478028445f67b508abef6b6a55408074
Amit Dahan <amitda@wix.com>
2020-03-08 21:55:59 +0200

    Fix Monaco styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
 import Head from 'next/head';
-import css from './index.module.css';
 import Editor from '../editor';
 
 const sampleCode = `function foo(): 'bar' | 'baz' {
@@ -9,11 +8,35 @@ const sampleCode = `function foo(): 'bar' | 'baz' {
 `;
 
 const Home: FC = () => (
-  <div className={css.root}>
+  <div className="root">
     <Head>
       <title>Code It!</title>
     </Head>
-    <Editor value={sampleCode} height={300} width={600} language="typescript" />
+    <Editor value={sampleCode} language="typescript" theme="vs-dark" />
+
+    <style jsx>{`
+      .root {
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: stretch;
+        align-items: stretch;
+      }
+    `}</style>
+
+    <style jsx global>{`
+      html,
+      body {
+        padding: 0;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      }
+
+      * {
+        box-sizing: border-box;
+      }
+    `}</style>
   </div>
 );
 
