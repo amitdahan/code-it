9cecf1fcc02d9a7bb850dad316e593cfe1d4cd9d
Amit Dahan <amitda@wix.com>
2020-03-08 16:04:26 +0200

    Initial editor work

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const withCSS = require("@zeit/next-css");
+const withFonts = require('next-fonts');
+
+module.exports = withFonts(withCSS({
+  webpack(config) {
+    config.plugins.push(
+      new MonacoWebpackPlugin()
+    );
+
+    return config;
+  }
+}));
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
 import { FC } from 'react';
 import Head from 'next/head';
 import css from './index.module.css';
+import Editor from '../editor';
+
+const sampleCode = `function foo(): 'bar' | 'baz' {
+  return Math.random() > 0.5 ? 'bar' : 'baz';
+}
+`;
 
 const Home: FC = () => (
   <div className={css.root}>
     <Head>
       <title>Code It!</title>
     </Head>
+    <Editor value={sampleCode} height={300} width={300} language="javascript" />
   </div>
 );
 
