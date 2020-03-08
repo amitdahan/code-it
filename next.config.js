1c8bd3efa3b9aef085a1d792b8877c021d621f7a
Amit Dahan <amitda@wix.com>
2020-03-08 17:34:25 +0200

    Refactor next.config.js

--- next.config.js
+++ next.config.js
@@ -2,16 +2,20 @@ const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 const withCSS = require('@zeit/next-css');
 const withFonts = require('next-fonts');
 
-module.exports = withFonts(
-  withCSS({
-    webpack(config) {
-      config.plugins.push(
-        new MonacoWebpackPlugin({
-          filename: 'static/[name].worker.js'
-        })
-      );
+const withMonaco = (nextConfig = {}) =>
+  withFonts(
+    withCSS({
+      ...nextConfig,
+      webpack(config) {
+        config.plugins.push(
+          new MonacoWebpackPlugin({
+            filename: 'static/[name].worker.js'
+          })
+        );
 
-      return config;
-    }
-  })
-);
+        return config;
+      }
+    })
+  );
+
+module.exports = withMonaco();
