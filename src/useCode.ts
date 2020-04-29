fa8843f5f15f7b2443d5952027436d024b408e49
Amit Dahan <amitda@wix.com>
2020-04-29 17:04:14 +0300

    Throttle code pushes

--- src/useCode.ts
+++ src/useCode.ts
@@ -2,15 +2,20 @@ import fetch from 'isomorphic-unfetch';
 import useSWR, { mutate } from 'swr';
 import usePusher from './usePusher';
 import { useCallback, useMemo } from 'react';
+import throttle from 'lodash/throttle';
 
-const saveCode = (id: string, code: string) =>
-  Promise.all([
+const mutateCode = (id: string, code: string) =>
+  mutate(`/api/get/${id}`, code, false);
+
+const saveCode = throttle(
+  (id: string, code: string) =>
     fetch(`/api/set/${id}`, {
       method: 'post',
       body: code,
     }),
-    mutate(`/api/get/${id}`, code, false),
-  ]);
+  1000,
+  { leading: false }
+);
 
 const useCode = (id: string, initialData: string) => {
   const { data: code } = useSWR(`/api/get/${id}`, {
@@ -18,11 +23,12 @@ const useCode = (id: string, initialData: string) => {
     fetcher: (url) => fetch(url).then((res) => res.text()),
   });
 
-  usePusher(`code-${id}`, 'change', (code: string) =>
-    mutate(`/api/get/${id}`, code, false)
-  );
+  usePusher(`code-${id}`, 'change', (code: string) => mutateCode(id, code));
 
-  const setCode = useCallback((code: string) => saveCode(id, code), [id]);
+  const setCode = useCallback(
+    (code: string) => Promise.all([mutateCode(id, code), saveCode(id, code)]),
+    [id]
+  );
 
   return useMemo(() => ({ code, setCode }), [code, setCode]);
 };
